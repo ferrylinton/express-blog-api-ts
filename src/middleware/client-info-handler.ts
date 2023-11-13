0a54531826e254441f9aebd1e49a17ee2afe3967
Ferry L.H <ferrylinton@gmail.com>
2023-11-13 13:54:17 +0700

    update clientIp

--- src/middleware/client-info-handler.ts
+++ src/middleware/client-info-handler.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 export const clientInfoHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const hostIp = req.header("x-forwarded-for") || '';
-        const clientIp = req.header("x-client-ip") || '';
+        const clientIp = req.header("x-client-ip") || hostIp || '';
         const userAgent = req.headers['user-agent'];
         req.client = {
             hostIp,
