75609568782e9c193a62c4b2a5d2c1c276b52240
Ferry L.H <ferrylinton@gmail.com>
2024-01-22 15:14:58 +0700

    update clientInfoHandler

--- src/middleware/client-info-handler.ts
+++ src/middleware/client-info-handler.ts
@@ -3,9 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 
 export const clientInfoHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const hostIp = req.header("x-forwarded-for") || '';
-        const clientIp = req.header("x-client-ip") || hostIp || '';
-        const userAgent = req.headers['user-agent'];
+        const hostHeader = req.header("x-forwarded-host") || 'unknown';
+        const hosts = hostHeader.split(",");
+        const hostIp = hosts.length > 0 ? hosts[0] : 'unknown';
+
+        const clientIp = req.header("x-client-ip") || req.header("x-real-ip") || req.header("x-forwarded-for") || 'unknown';
+        const userAgent = req.headers['user-agent'] || 'unknown';
         req.client = {
             hostIp,
             clientIp,
